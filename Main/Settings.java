5c52a7dc73ea9503fad41fb11a779ed480ac2511
David Wager <maddavid123@gmail.com>
2018-09-25 14:55:40 +0100

    0.1
    What I got

---
+++ Main/Settings.java
@@ -0,0 +1,220 @@
+package Main;
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * Created by David on 14/03/2018.
+ */
+public class Settings {
+    public static String HOST = "irc.twitch.tv";
+    public static int PORT = 6667;
+    public static String PASS; //Remember to snip
+    public static String NICK; //Remember to snip
+    public static String CHANNEL; //Remember to snip
+    public static ArrayList<Sound> soundList = new ArrayList<>();
+    public static ArrayList<String> modsList = new ArrayList<>();
+    private static boolean dbExist = false;
+
+    public static void setupSounds(){
+        dbExist = testDB();
+        if(!dbExist){
+            java.sql.Connection c = null;
+            try{
+                Class.forName("org.sqlite.JDBC");
+                c = DriverManager.getConnection("jdbc:sqlite:database.db");
+                Statement s  = c.createStatement();
+                String sql = "CREATE TABLE Sound(\n" +
+                        "soundCommand   STRING  PRIMARY KEY,\n" +
+                        "filepath       STRING  NOT NULL,\n" +
+                        "accessType     STRING  NOT NULL,\n" +
+                        "cooldown       INT     NOT NULL\n" +
+                        ");";
+                s.execute(sql);
+                sql = "CREATE TABLE Mod(Username   String  PRIMARY KEY);";
+                s.execute(sql);
+                s.close();
+                dbExist = true;
+            } catch(SQLException e){
+                e.printStackTrace();
+            } catch(ClassNotFoundException e){
+                System.err.println("Don't know about that class.");
+            }
+            finally {
+                try{
+                    if(c != null){
+                        c.close();
+                    }
+                }catch(SQLException e){
+                    System.err.println("Couldn't connect to database");
+                }
+            }
+        }
+        if(dbExist){
+            java.sql.Connection c = null;
+            try{
+                Class.forName("org.sqlite.JDBC");
+                c = DriverManager.getConnection("jdbc:sqlite:database.db");
+                Statement s  = c.createStatement();
+                String sql = "SELECT *  FROM Sound, Mod";
+                ResultSet rs = s.executeQuery(sql);
+                while(rs.next()){
+                    soundList.add(new Sound(rs.getString("soundCommand"),rs.getString("filepath"),rs.getString("accessType"), rs.getInt("cooldown")));
+                    modsList.add(rs.getString("Username"));
+                }
+                s.close();
+            } catch(SQLException e){
+                e.printStackTrace();
+            } catch(ClassNotFoundException e){
+                System.err.println("Don't know about that class.");
+            }
+            finally {
+                try{
+                    if(c != null){
+                        c.close();
+                    }
+                }catch(SQLException e){
+                    System.err.println("Couldn't connect to database");
+                }
+            }
+        }
+        modsList.add(CHANNEL);
+    }
+    public static void addNewSound(Sound s){
+        if(dbExist){
+            soundList.add(s);
+            java.sql.Connection c = null;
+            try{
+                Class.forName("org.sqlite.JDBC");
+                c = DriverManager.getConnection("jdbc:sqlite:database.db");
+                PreparedStatement stmn = c.prepareStatement("INSERT INTO Sound\n" +
+                        "VALUES(?,?,?,?);");
+                stmn.setString(1,s.getSoundCommand());
+                stmn.setString(2,s.getFilePath());
+                stmn.setString(3,s.getAccessType());
+                stmn.setFloat(4,s.getDefaultCooldown());
+                stmn.execute();
+                stmn.close();
+                c.commit();
+            } catch(SQLException e){
+                e.printStackTrace();
+            } catch(ClassNotFoundException e){
+                System.err.println("Don't know about that class.");
+            }
+            finally {
+                try{
+                    if(c != null){
+                        c.close();
+                    }
+                }catch(SQLException e){
+                    System.err.println("Couldn't connect to database");
+                }
+            }
+        }
+    }
+    public static void editSound(Sound s){
+        if(dbExist){
+            for(int i = 0; i<soundList.size();i++){
+                Sound savedSound = soundList.get(i);
+                if(savedSound.getSoundCommand().equals(s.getSoundCommand())){
+                    soundList.remove(i);
+                    soundList.add(s);
+                }
+            }
+            java.sql.Connection c = null;
+            try{
+                Class.forName("org.sqlite.JDBC");
+                c = DriverManager.getConnection("jdbc:sqlite:database.db");
+                PreparedStatement stmn = c.prepareStatement("UPDATE Sound\n" +
+                        "SET soundCommand=?,filepath=?,accessType=?,volume=?\n" +
+                        "WHERE soundCommand=?");
+                stmn.setString(1,s.getSoundCommand());
+                stmn.setString(2,s.getFilePath());
+                stmn.setString(3,s.getAccessType());
+                stmn.setFloat(4,s.getDefaultCooldown());
+                stmn.setString(5,s.getSoundCommand());
+                stmn.execute();
+                stmn.close();
+                c.commit();
+            } catch(SQLException e){
+                e.printStackTrace();
+            } catch(ClassNotFoundException e){
+                System.err.println("Couldn't bridge connection to database.");
+            }
+            finally {
+                try{
+                    if(c != null){
+                        c.close();
+                    }
+                }catch(SQLException e){
+                    System.err.println("Couldn't connect to database");
+                }
+            }
+        }
+    }
+    public static void deleteSound(String soundCommand){
+        if(dbExist){
+            for(int i = 0; i<soundList.size();i++){
+                Sound savedSound = soundList.get(i);
+                if(savedSound.getSoundCommand().equals(soundCommand)){
+                    soundList.remove(i);
+                }
+            }
+            java.sql.Connection c = null;
+            try{
+                Class.forName("org.sqlite.JDBC");
+                c = DriverManager.getConnection("jdbc:sqlite:database.db");
+                PreparedStatement stmn = c.prepareStatement("DELETE FROM Sound\n" +
+                        "WHERE soundCommand=?");
+                stmn.setString(5,soundCommand);
+                stmn.execute();
+                stmn.close();
+                c.commit();
+            } catch(SQLException e){
+                e.printStackTrace();
+            } catch(ClassNotFoundException e){
+                System.err.println("Don't know about that class.");
+            }
+            finally {
+                try{
+                    if(c != null){
+                        c.close();
+                    }
+                }catch(SQLException e){
+                    System.err.println("Couldn't connect to database");
+                }
+            }
+        }
+    }
+    public static boolean testDB(){
+        java.sql.Connection c = null;
+        boolean tableExist = true;
+        try{
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:database.db");
+            Statement s  = c.createStatement();
+            String sql = "INSERT INTO Sound\n" +
+                    "VALUES('doubleTest','test','Admin','50');";
+            s.execute(sql);
+            sql = "DELETE FROM Sound\n" +
+                    "WHERE filepath = 'test'";
+            s.execute(sql);
+            s.close();
+        } catch(SQLException e){
+            tableExist = false;
+        } catch(ClassNotFoundException e){
+            System.err.println("Don't know about that class.");
+        }
+        finally {
+            try{
+                if(c != null){
+                    c.close();
+                }
+            }catch(SQLException e){
+                System.err.println("Couldn't connect to database");
+            }
+        }
+        return tableExist;
+    }
+
+
+}
---
+++ Main/Sound.java
@@ -0,0 +1,65 @@
+package Main;
+
+/**
+ * Created by David on 14/03/2018.
+ */
+public class Sound {
+    private String soundCommand;
+    private String filePath;
+    private String accessType;
+    private int cooldown;
+    private int defaultCooldown;
+    private int lastUsed;
+
+    public Sound(String soundCommand, String filePath, String accessType, int cooldown) {
+        this.soundCommand = soundCommand;
+        this.filePath = filePath;
+        this.accessType = accessType;
+        this.cooldown = cooldown;
+        defaultCooldown = cooldown;
+    }
+
+    public int getCooldown() {
+        return cooldown;
+    }
+
+    public void setCooldown(int cooldown) {
+        this.cooldown = cooldown;
+    }
+
+    public String getSoundCommand() {
+        return soundCommand;
+    }
+
+    public void setSoundCommand(String soundCommand) {
+        this.soundCommand = soundCommand;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getAccessType() {
+        return accessType;
+    }
+
+    public void setAccessType(String accessType) {
+        this.accessType = accessType;
+    }
+
+    public int getLastUsed() {
+        return lastUsed;
+    }
+
+    public void setLastUsed(int lastUsed) {
+        this.lastUsed = lastUsed;
+    }
+
+    public int getDefaultCooldown() {
+        return defaultCooldown;
+    }
+}
